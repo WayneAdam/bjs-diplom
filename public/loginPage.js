e4a200125f6b761088bc15b020877527b2fd7b0d
Wayne <meibeat@mail.ru>
2023-08-06 16:25:29 +0300

    Работа с loginPage - выполнена

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,7 +3,43 @@ console.log("Start script");
 
 // создает объект класса UserForm
 const userFormObject = new UserForm();
-// присваивает свойству loginFormCallback объекта userFormObject значение функции
+
+/** 
+ * Вход 
+ */
 userFormObject.loginFormCallback = (data) => {
-  const request = fetch();
+  ApiConnector.login(data, response => {
+    // выводит в консоль ответ от сервера
+    console.log(response);
+
+    // проверяет успешность запроса
+    if(response.success === true) {
+      location.reload();
+    } else {
+      console.log(response.error);
+      return alert(response.error);
+    };
+
+  });
+
+};
+
+/** 
+ * Регистрация
+ */
+userFormObject.registerFormCallback = (data) => {
+  ApiConnector.register(data, response => {
+    // выводит в консоль ответ от сервера
+    console.log(response);
+
+    // проверяет повторную регистрацию
+    if(response.success === false) {
+      console.log(response.error);
+      return alert(response.error);
+    } else {
+      location.reload();
+    };
+
+  });
+
 };
